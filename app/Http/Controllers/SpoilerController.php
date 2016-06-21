3f2c41f43e73fc7a8a37eec1b4227a0f9d4faa99
kodumen <r.b.daitan@gmail.com>
2016-06-22 02:38:03 +0800

    TEST

--- app/Http/Controllers/SpoilerController.php
+++ app/Http/Controllers/SpoilerController.php
@@ -9,10 +9,6 @@ class SpoilerController extends Controller
     public function format(Request $request)
     {
         $formatter = new MessageFormatter;
-        
-        header('content-type: application/json');
-        echo json_encode($formatter->format($request->get('text')));
-        die;
 
         return response(json_encode($formatter->format($request->get('text'))))->header('content-type', 'application/json');
     }
