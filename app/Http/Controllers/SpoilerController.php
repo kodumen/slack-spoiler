a3cf1a148cbf4875a293dc66bccf750fc2f4d449
kodumen <r.b.daitan@gmail.com>
2016-06-22 02:58:22 +0800

    TEST

--- app/Http/Controllers/SpoilerController.php
+++ app/Http/Controllers/SpoilerController.php
@@ -3,13 +3,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Library\MessageFormatter;
+use GuzzleHttp\Client;
 
 class SpoilerController extends Controller
 {
     public function format(Request $request)
     {
         $formatter = new MessageFormatter;
+        $message = $formatter->format($request->get('text'));
 
-        return response(json_encode($formatter->format($request->get('text'))))->header('content-type', 'application/json');
+        $client = new Client();
+        $client->request('POST', $request->get('response_url'), ['json' => $message]);
+
+        // return response(json_encode($formatter->format($request->get('text'))))->header('content-type', 'application/json');
+        return ''
     }
 }
