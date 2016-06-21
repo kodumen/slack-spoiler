36274759c8c363389a52363fcedaa6cf60b1b5b3
kodumen <r.b.daitan@gmail.com>
2016-06-22 00:45:28 +0800

    Use
    yaydadyadya

--- app/Http/Controllers/SpoilerController.php
+++ app/Http/Controllers/SpoilerController.php
@@ -2,11 +2,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Library\MessageFormatter;
 
 class SpoilerController extends Controller
 {
     public function format(Request $request)
     {
-        var_dump($request->all());
+        $formatter = new MessageFormatter;
+        
+        return response()->json(
+            $formatter->format($request->get('text'))
+        );
     }
 }
